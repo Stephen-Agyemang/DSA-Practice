12988f3836b464ba22c9dfd3a3f17067a3dc8b2a
Stephen Agyemang <agyemangstephen2580@gmail.com>
2025-09-19 20:38:24 +0000

    Sync LeetCode submission Runtime - 4 ms (40.82%), Memory - 19 MB (100.00%)

--- 0001-two-sum/solution.py
+++ 0001-two-sum/solution.py
@@ -1,8 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]: 
-        for i in range(0, len(nums)):
-            for j in range(i + 1, len(nums)):
-                sum = nums[i] + nums[j]
-                if sum == target:
-                    return [i, j] 
+        hash_map = {}
+
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in hash_map:
+                return [hash_map[complement], i]
+            hash_map[num] = i
         return []
