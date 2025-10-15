3ca11abc311d93c622bea780c336524652cf2bbc
Stephen Agyemang <agyemangstephen2580@gmail.com>
2025-10-15 19:27:27 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.6 MB (100.00%)

--- 1528-kids-with-the-greatest-number-of-candies/solution.py
+++ 1528-kids-with-the-greatest-number-of-candies/solution.py
@@ -1,18 +1,18 @@
 class Solution:
     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
-        
-        max_candies = max(candies)
-        grtst_candies = []
 
-        for n in range(len(candies)):
-            if candies[n] + extraCandies >= max_candies:
-                grtst_candies.append(True)
+        max_candies = max(candies) 
+        result = [] 
+
+        for candy in candies:
+            if candy + extraCandies >= max_candies:
+                result.append(True)
+
             else:
-                grtst_candies.append(False)
-        return grtst_candies
+                result.append(False)
         
-
-
+        return result
+        
 
 
         
