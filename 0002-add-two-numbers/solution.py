dcaea0cdbb0565384672985290468eade1a180c6
Stephen Agyemang <agyemangstephen2580@gmail.com>
2026-06-16 21:21:44 +0000

    Sync LeetCode submission Runtime - 3 ms (65.79%), Memory - 19.3 MB (77.99%)

--- 0002-add-two-numbers/solution.py
+++ 0002-add-two-numbers/solution.py
@@ -3,58 +3,87 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-def reverse_ll(head):
 
-        if not head:
-            return None
+# def reverse_ll(head):
 
-        curr = head
-        prev = None
+#         if not head:
+#             return None
 
-        while curr:
-            next_node = curr.next
-            curr.next = prev
+#         curr = head
+#         prev = None
 
-            prev = curr
-            curr = next_node
-        return prev 
+#         while curr:
+#             next_node = curr.next
+#             curr.next = prev
+
+#             prev = curr
+#             curr = next_node
+#         return prev 
 
 class Solution:
 
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
 
-        curr_1 = reverse_ll(l1)
-        curr_2 = reverse_ll(l2)
-        str_1 = ""
-        str_2 = ""
+        # curr_1 = reverse_ll(l1)
+        # curr_2 = reverse_ll(l2)
+        # str_1 = ""
+        # str_2 = ""
 
-        while curr_1:
-            str_1 += str(curr_1.val)
-            curr_1 = curr_1.next
+        # while curr_1:
+        #     str_1 += str(curr_1.val)
+        #     curr_1 = curr_1.next
 
-        while curr_2:
-            str_2 += str(curr_2.val)
-            curr_2 = curr_2.next
+        # while curr_2:
+        #     str_2 += str(curr_2.val)
+        #     curr_2 = curr_2.next
 
-        print(str_1)
-        print(str_2)
+        # answer = int(str_1) + int(str_2)
 
-        answer = int(str_1) + int(str_2)
-        print(answer)
+        # answer_list = list(str(answer))
 
-        answer_list = list(str(answer))
-        print(answer)
+        # newHead = ListNode(int(answer_list.pop()))
+        # curr = newHead
 
-        newHead = ListNode(int(answer_list.pop()))
-        curr = newHead
+        # while answer_list:
+        #     digit = int(answer_list.pop())
+        #     curr.next = ListNode(digit)
+        #     curr = curr.next
 
-        while answer_list:
-            digit = int(answer_list.pop())
-            curr.next = ListNode(digit)
-            curr = curr.next
+        # return newHead
 
-        return newHead
+        dummy = ListNode(-1)
+        current = dummy
+
+        carry = 0 
+
+        while l1 or l2 or carry:
+
+            if l1:
+                val1 = l1.val 
+            else:
+                val1 = 0
+
+            if l2:
+                val2 = l2.val
+            else:
+                val2 = 0 
+
+            total = val1 + val2 + carry
+
+            carry = total // 10
+            digit = total % 10
+
+            current.next = ListNode(digit) 
+            current = current.next
+
+            if l1:
+                l1 = l1.next
             
+            if l2:
+                l2 = l2.next 
+
+        return dummy.next
+
 
 
 
