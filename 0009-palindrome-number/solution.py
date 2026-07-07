18ed816a4d7b09819e29be22fc71e756614c56a9
Stephen Agyemang <agyemangstephen2580@gmail.com>
2026-07-07 02:20:16 +0000

    Sync LeetCode submission Runtime - 4 ms (80.55%), Memory - 19.3 MB (17.60%)

--- 0009-palindrome-number/solution.py
+++ 0009-palindrome-number/solution.py
@@ -1,15 +1,22 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         
-        if x < 0:
+       # Using math to solve this question
+
+        if x < 0 or (x % 10 == 0 and x != 0):
             return False
 
-        original = x 
-        reversed_num = 0
+        reversed_num = 0 
+        original_num = x
 
         while x > 0:
-            digit = x % 10
-            reversed_num = reversed_num * 10 + digit 
-            x //= 10
+            last_digit = x % 10
 
-        return original == reversed_num
+            reversed_num = (reversed_num * 10) + last_digit
+            x = x // 10
+
+        return reversed_num == original_num
+
+
+    
+    
