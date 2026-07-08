46abdfbb23c22bf0770197254aa0c606e3b6248e
Stephen Agyemang <agyemangstephen2580@gmail.com>
2026-07-08 02:18:50 +0000

    Sync LeetCode submission Runtime - 17 ms (41.46%), Memory - 39 MB (32.21%)

--- 0334-increasing-triplet-subsequence/solution.py
+++ 0334-increasing-triplet-subsequence/solution.py
@@ -2,13 +2,23 @@ from math import inf
 
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
-        first = second = inf
+
+        first = inf 
+        second = inf
 
         for num in nums:
             if num <= first:
                 first = num
+
             elif num <= second:
                 second = num
+            
             else:
                 return True
+
         return False
+
+            
+        
+
+      
