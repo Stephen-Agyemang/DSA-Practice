62731249b282431c11c27c35423b71087bda9b6a
Stephen Agyemang <agyemangstephen2580@gmail.com>
2026-07-09 01:49:04 +0000

    Sync LeetCode submission Runtime - 3 ms (85.26%), Memory - 21.2 MB (49.77%)

--- 0169-majority-element/solution.py
+++ 0169-majority-element/solution.py
@@ -1,19 +1,17 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        hashMap = {}
-        max_count = 0
-        maj_element = 0
-        
-        for num in nums:
-            if num in hashMap:
-                hashMap[num] += 1
-            
-            else:
-                hashMap[num] = 1
 
-            if hashMap[num] > max_count:
-                max_count = hashMap[num]
-                maj_element = num
-        
-        return maj_element
-        
+        candidate = None
+        count = 0 
+
+        for num in nums:
+            if count == 0:
+                candidate = num 
+
+            if num == candidate:
+                count += 1
+            else:
+                count -= 1
+
+        return candidate
+
