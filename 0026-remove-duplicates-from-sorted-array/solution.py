9b96315b641b5b6de9c50427b0ae3af957c07ae3
Stephen Agyemang <agyemangstephen2580@gmail.com>
2026-07-09 02:48:31 +0000

    Sync LeetCode submission Runtime - 3 ms (45.63%), Memory - 20.6 MB (16.47%)

--- 0026-remove-duplicates-from-sorted-array/solution.py
+++ 0026-remove-duplicates-from-sorted-array/solution.py
@@ -1,10 +1,21 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        
-        k = 0
 
-        for i in range(len(nums)):
-            if nums[k] < nums[i]:
-                nums[k+1] = nums[i]
-                k += 1
-        return k + 1
+        if not nums:
+            return 0 
+
+        
+        write = 1
+
+        for i in range(1, len(nums)):
+
+            if nums[i] != nums[i-1]:
+                nums[write] = nums[i] 
+                write += 1
+
+        return write
+
+
+
+        
+        
