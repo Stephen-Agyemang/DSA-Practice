3fa91f861ad1173d7954c46f999db140b0050623
Stephen Agyemang <agyemangstephen2580@gmail.com>
2026-07-10 01:48:15 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.2 MB (56.59%)

--- 0027-remove-element/solution.py
+++ 0027-remove-element/solution.py
@@ -1,12 +1,27 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        
-        k = 0 
 
-        for i in range(len(nums)):
-            if nums[i] != val:
-                nums[k] = nums[i] 
-                k += 1
-        return k
-        
+        if not nums:
+            return 0
+
+        right = len(nums) - 1
+        left = 0
+
+        while left <= right:
+            if nums[left] == val and nums[right] != val:
+                nums[right], nums[left] = nums[left], nums[right] 
+                right -= 1
+                left += 1
+
+            elif nums[left] != val and nums[right] == val:
+                left += 1
+                right -= 1
+
+            elif nums[left] != val and nums[right] != val:
+                left += 1
+
+            else:
+                right -= 1
+
+        return left
         
