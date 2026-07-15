03bc71bb4ba97decf52e5c373bb4ad0126e6eff6
Stephen Agyemang <agyemangstephen2580@gmail.com>
2026-07-15 02:05:21 +0000

    Sync LeetCode submission Runtime - 2 ms (37.36%), Memory - 19.2 MB (96.52%)

--- 0349-intersection-of-two-arrays/solution.py
+++ 0349-intersection-of-two-arrays/solution.py
@@ -1,4 +1,10 @@
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        results = set()
+        set1 = set(nums1)
 
-        return list(set(nums1) & set(nums2))
+        for num in nums2: 
+            if num in set1:
+                results.add(num)
+
+        return list(results)
