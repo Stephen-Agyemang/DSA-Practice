a3830f6b2e6b1ba5272752856553477e49fa3f22
Stephen Agyemang <agyemangstephen2580@gmail.com>
2026-07-16 01:14:51 +0000

    Sync LeetCode submission Runtime - 1 ms (26.50%), Memory - 19.3 MB (77.34%)

--- 2058-concatenation-of-array/solution.py
+++ 2058-concatenation-of-array/solution.py
@@ -1,5 +1,5 @@
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
         
-        nums = nums + nums
-        return nums 
+        nums.extend(nums)
+        return nums
