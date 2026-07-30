47ffb8543f48e52c2da40361ee14b5104869643a
Stephen Agyemang <agyemangstephen2580@gmail.com>
2026-07-30 01:35:23 +0000

    Sync LeetCode submission Runtime - 3 ms (30.72%), Memory - 19.7 MB (87.91%)

---
+++ 0414-third-maximum-number/solution.py
@@ -0,0 +1,30 @@
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+
+        if not nums:
+            return -1 
+
+        first = second = third = float('-inf')
+
+        for num in nums:
+
+            if num in (first, second, third):
+                continue
+
+            if num > first: 
+                first, second, third = num, first, second
+
+            elif num > second:
+                second, third = num, second
+
+            elif num > third:
+                third = num
+
+            
+        if third != float('-inf'):
+            return third
+
+        else:
+            return first
+    
+        
