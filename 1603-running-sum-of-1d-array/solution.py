5127a5bb70b0092509895c6c18f8e6c1430c41ef
Stephen Agyemang <agyemangstephen2580@gmail.com>
2026-08-03 00:04:02 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.4 MB (10.70%)

---
+++ 1603-running-sum-of-1d-array/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+
+        if not nums:
+            return None
+    
+        for i in range(1, len(nums)):
+            nums[i] += nums[i-1]
+
+        return nums
+        
