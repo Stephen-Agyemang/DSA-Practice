e5dad7f12526b1bf491b136b95be775511bbed74
Stephen Agyemang <agyemangstephen2580@gmail.com>
2026-08-07 05:01:02 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.3 MB (68.36%)

--- 0118-pascals-triangle/solution.py
+++ 0118-pascals-triangle/solution.py
@@ -4,14 +4,27 @@ class Solution:
         pascal_list = []
 
         for i in range(numRows):
+
             row = [1] * (i + 1)
 
             for j in range(1, i):
                 prev_row = pascal_list[i - 1]
-                row[j] = prev_row[j] + prev_row[j-1]
+                row[j] = prev_row[j] + prev_row[j - 1]
 
             pascal_list.append(row)
 
         return pascal_list
 
-      
+            
+
+
+    
+
+
+
+
+
+
+
+
+     
