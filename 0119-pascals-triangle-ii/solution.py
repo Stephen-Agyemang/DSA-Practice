4833c4037be6640eddde84ca7fac3df9661f664c
Stephen Agyemang <agyemangstephen2580@gmail.com>
2026-08-07 05:27:33 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.3 MB (22.20%)

--- 0119-pascals-triangle-ii/solution.py
+++ 0119-pascals-triangle-ii/solution.py
@@ -1,11 +1,22 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
-        
-        row = [1] * (rowIndex + 1)
 
+        row = [1] * (rowIndex + 1)
+        
         for i in range(1, rowIndex):
 
             for j in range(i, 0, -1):
                 row[j] = row[j] + row[j-1]
 
         return row
+
+
+    #               0  1  2  3 
+    #     output = [1, 3, 3, 1]
+
+    #            0  1  2  3
+    #     row = [1, 1, 1, 1]
+
+            
+    # #  0      1     2
+    # # [[1], [1,1], [1, 2, 1], [1, 3, 3, 1]]
