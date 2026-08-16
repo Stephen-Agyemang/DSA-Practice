15973ddfc7a2d7d42b4567a97e44b64df1d4e111
Stephen Agyemang <agyemangstephen2580@gmail.com>
2026-08-16 20:45:39 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.1 MB (98.89%)

--- 1552-build-an-array-with-stack-operations/solution.py
+++ 1552-build-an-array-with-stack-operations/solution.py
@@ -1,26 +1,28 @@
 class Solution:
     def buildArray(self, target: List[int], n: int) -> List[str]:
-        ## Target is increasing(it is an array of numbers)
+        ## Target is increasing (it is an array of numbers)
 
         operations = []
         left = 0  # Left here represents the indices of the numbers in target
 
 
 
-        for i in range(1, n + 1):   
-            
+        for i in range(1, n + 1):  
+
             if len(target) == left:
                 break
-
 
             if target[left] == i:
                 operations.append("Push")
                 left += 1
 
-            else: 
+            else:
                 operations.append("Push")
-                operations.append("Pop") 
+                operations.append("Pop")
+
         return operations
+
+        
 
         
 
