a1343b80c18c02bb895a7d5f298e0d5018ce3f4c
Stephen Agyemang <agyemangstephen2580@gmail.com>
2026-08-24 00:53:57 +0000

    Sync LeetCode submission Runtime - 4 ms (27.12%), Memory - 19.3 MB (84.17%)

--- 0837-most-common-word/solution.py
+++ 0837-most-common-word/solution.py
@@ -1,32 +1,45 @@
-import re 
-from typing import List
+import re
+from collections import Counter
 
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
 
-        dct = {}
-        banned_word = set(banned)
+        # let say am able to make paragraph into a dctionary of without adding any of the stuff that are not needed like the symbols and also with everything in lowercase
 
+        # The next parts of what I would do is creating a string word, which will have the string to return
+        # I will also have max_freq which would be the freq of that word not to return but guide in finding the word to return
 
-        clean_paragraph = re.sub(r'[^\w\s]', ' ', paragraph.lower())
-        words = clean_paragraph.split()
+        # I will go through the dictionary, the word I encounter is in there set of banned_words, which is set(banned), for O(1) look-up, I would continue or skip it 
 
-        for word in words:
+        # I will also use the iteration to update the values of both max_freq and word to find the word with the highest value or dct_paragraph[char] and store it everytime until the process is over which will end up getting me the word I need.
 
-            if word in banned_word:
-                continue
-           
-            if word in dct:
-                dct[word] += 1
+        # words = re.findall(r'\w+', paragraph.lower()) # This converts paragraph into a list of all accepted words.
+
+        # # or we can do this
+
+        cleaner = []
+
+        for char in paragraph:
+            if char.isalnum():
+                cleaner.append(char)
+
             else:
-                dct[word] = 1
+                cleaner.append(" ")
 
-        print(dct)
-        max_appearance = ""
-        freq = 0
-        for key in dct:
-            if dct[key] > freq:
-                freq = dct[key]
-                max_appearance = key 
+        clean_string = "".join(cleaner)
 
-        return max_appearance
+        words = clean_string.lower().split()
+
+        set_banned = set(banned)
+        counts = Counter(words)
+
+        most_common = ""
+        max_freq = 0 
+
+        for word in counts:
+            if word not in set_banned and counts[word] > max_freq:
+                most_common = word 
+                max_freq = counts[word] 
+
+        return most_common
+
