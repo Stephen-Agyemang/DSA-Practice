7ca95258a6e8348302e53c1f76943ffd6d094a72
Stephen Agyemang <agyemangstephen2580@gmail.com>
2026-08-25 04:59:20 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.3 MB (22.93%)

--- 4252-first-unique-even-element/solution.py
+++ 4252-first-unique-even-element/solution.py
@@ -1,25 +1,30 @@
+from collections import Counter
 class Solution:
     def firstUniqueEven(self, nums: list[int]) -> int:
-        
-        dct = {}
-        
+
+        # dct = {}
+
+        # for i, num in enumerate(nums):
+        #     if num not in dct and num % 2 == 0:
+        #         dct[num] = i 
+
+        #     elif num in dct:
+        #         dct[num] = -1
+
+        # min_index = 100
+        # even_number = -1
+
+        # for key in dct:
+        #     if dct[key] < min_index and dct[key] >= 0:
+        #         even_number = key
+        #         min_index = dct[key]
+
+        # return even_number
+
+        counts = Counter(nums)
+
         for num in nums:
-            if num % 2 != 0:
-                continue
+            if num % 2 == 0 and counts[num] == 1:
+                return num
 
-            else:
-                if num not in dct and num % 2 == 0:
-                    dct[num] = 1
-
-                else:
-                    dct[num] += 1
-
-        
-        for key in dct:
-            if dct[key] == 1:
-                return key
-        
         return -1
-                
-            
-        
