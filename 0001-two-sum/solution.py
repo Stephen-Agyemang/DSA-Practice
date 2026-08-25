aa69b70ef183a3161030864f274356f069eb319a
Stephen Agyemang <agyemangstephen2580@gmail.com>
2026-08-25 00:59:36 +0000

    Sync LeetCode submission Runtime - 3 ms (53.66%), Memory - 20.4 MB (41.15%)

--- 0001-two-sum/solution.py
+++ 0001-two-sum/solution.py
@@ -1,6 +1,7 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        
+
+
         if not nums:
             return []
 
@@ -15,5 +16,8 @@ class Solution:
             dct[num] = i
 
         return []
+            
+            
+
             
         
