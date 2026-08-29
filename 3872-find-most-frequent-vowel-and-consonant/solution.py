7a0e2670c1d788f0ff2b7747d87d307217608e79
Stephen Agyemang <agyemangstephen2580@gmail.com>
2026-08-29 05:50:03 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.3 MB (63.35%)

--- 3872-find-most-frequent-vowel-and-consonant/solution.py
+++ 3872-find-most-frequent-vowel-and-consonant/solution.py
@@ -1,25 +1,23 @@
+from collections import Counter
+
 class Solution:
     def maxFreqSum(self, s: str) -> int:
+
+        vowel_freq = 0
+        consonant_freq = 0 
+
+        char_frequencies = Counter(s)
+
+        for key in char_frequencies:
+            if key in 'aeiou' and char_frequencies[key] > vowel_freq:
+                vowel_freq = char_frequencies[key]
+
+            elif key not in 'aeiou' and char_frequencies[key] > consonant_freq:
+                consonant_freq = char_frequencies[key]
+
+        return vowel_freq + consonant_freq
+
         
-        freq_consonant = 0
-        freq_vowel = 0
-        vowels = set("aeiou")
-        dct = {}
-
-        for letter in s:
-            if letter in dct:
-                dct[letter] += 1
-            
-            else:
-                dct[letter] = 1
-
-        for key in dct:
-            if key in vowels and dct[key] > freq_vowel:
-                freq_vowel = dct[key]
-
-            if key not in vowels and dct[key] > freq_consonant:
-                freq_consonant = dct[key]
-
-        return freq_vowel + freq_consonant
 
 
+        
