4c75c73adc231b9a6c3fbf969985c0a6478af00a
Stephen Agyemang <agyemangstephen2580@gmail.com>
2026-08-30 00:11:18 +0000

    Sync LeetCode submission Runtime - 115 ms (44.93%), Memory - 38 MB (77.36%)

--- 0160-intersection-of-two-linked-lists/solution.py
+++ 0160-intersection-of-two-linked-lists/solution.py
@@ -6,34 +6,27 @@
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        ## We want to get to the node where the point in the two linkedlist where the nodes point to the same in memory.
-        ## We might want to go through both separately or in a specific sequence so that we can get to that point in all the lists
-
-        if not headA and headB:
+    
+        if not headA or not headB:
             return None
 
-        curr_a = headA
-        curr_b = headB
 
-        dct = {}
+        pointerA = headA
+        pointerB = headB
 
-        while curr_a:
-            if curr_a not in dct:
-                dct[curr_a] = 1
 
-            else:
-                dct[curr_a] += 1
-
-            curr_a = curr_a.next
-
-        while curr_b:
-            if curr_b not in dct:
-                dct[curr_b] = 1
+        while pointerA != pointerB:
+            if pointerA:
+                pointerA = pointerA.next
 
             else:
-                return curr_b
-                
-            curr_b = curr_b.next
+                pointerA = headB
 
-        return None
+            if pointerB:
+                pointerB = pointerB.next
 
+            else:
+                pointerB = headA
+
+
+        return pointerA
