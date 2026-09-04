ebb20558aadccb329f6cffba78a3184fa8a0045f
Stephen Agyemang <agyemangstephen2580@gmail.com>
2026-09-04 21:32:52 +0000

    Sync LeetCode submission Runtime - 3 ms (91.72%), Memory - 19.2 MB (48.01%)

--- 0205-isomorphic-strings/solution.py
+++ 0205-isomorphic-strings/solution.py
@@ -1,33 +1,22 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-
+        
         if len(s) != len(t):
             return False
 
-        map_s_to_t = {}
-        map_t_to_s = {}
+        dct = {}
+        seen_stuff = set()
 
-
-        for i in range(len(s)):
-            char_s = s[i]
-            char_t = t[i]
-
-            if char_s in map_s_to_t:
-
-                if map_s_to_t[char_s] != char_t:
+        for i, char in enumerate(s):
+            if char not in dct:
+                if t[i] in seen_stuff:
                     return False
 
-            if char_t in map_t_to_s:
+                dct[char] = t[i]
+                seen_stuff.add(t[i])
 
-                if map_t_to_s[char_t] != char_s:
+            else:
+                if dct[char] != t[i]:
                     return False
-
-            map_s_to_t[char_s] = char_t
-            map_t_to_s[char_t] = char_s
-
         return True
-
-        
-
-        
         
