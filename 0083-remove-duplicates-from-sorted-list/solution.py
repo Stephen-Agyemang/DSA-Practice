b3c4a70d3f5053ce6d4420e2f2b93bf7dfeb50da
Stephen Agyemang <agyemangstephen2580@gmail.com>
2026-09-06 19:43:28 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19 MB (99.20%)

--- 0083-remove-duplicates-from-sorted-list/solution.py
+++ 0083-remove-duplicates-from-sorted-list/solution.py
@@ -5,15 +5,26 @@
 #         self.next = next
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        
-        curr = head
+        curr = head 
 
         while curr and curr.next:
-        
-            if curr.val == curr.next.val:
+            if curr.next.val == curr.val:
                 curr.next = curr.next.next
-            
+
             else:
                 curr = curr.next
-        
+
         return head
+
+
+        # dummy = ListNode(float('inf'))
+        # curr_dummy = dummy
+
+        # while head:
+        #     if head.val != curr_dummy.val:
+        #         curr_dummy.next = ListNode(head.val)
+        #         curr_dummy = curr_dummy.next
+
+        #     head = head.next
+
+        # return dummy.next
