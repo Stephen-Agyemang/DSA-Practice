2da60f46ce6281231fa74957e6b2ae5a35718339
Stephen Agyemang <agyemangstephen2580@gmail.com>
2026-09-09 01:35:51 +0000

    Sync LeetCode submission Runtime - 3 ms (63.06%), Memory - 19.3 MB (40.50%)

--- 1297-maximum-number-of-balloons/solution.py
+++ 1297-maximum-number-of-balloons/solution.py
@@ -1,24 +1,20 @@
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
 
-        if not text:
+        balloon = {'b': 0, 'a': 0, 'l': 0, 'o': 0, 'n': 0}
+
+        if len(text) < len(balloon):
             return 0
 
-        dct = {'b': 0,  'a': 0, 'l': 0, 'o': 0, 'n': 0}
-
         for char in text:
-            if char in dct:
-                dct[char] += 1
-
-        print(dct)
-
-        return min(
-            dct['b'] // 1, 
-            dct['a'] // 1,
-            dct['l'] // 2,
-            dct['o'] // 2,
-            dct['n'] // 1
-        )
-
+            if char in balloon:
+                balloon[char] += 1
 
         
+        return min(
+            balloon['b'] // 1,
+            balloon['a'] // 1,
+            balloon['l'] // 2,
+            balloon['o'] // 2,
+            balloon['n'] // 1,
+        )
