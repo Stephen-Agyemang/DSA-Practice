b3e39b53a38bcaebff167d7cf54fc55f7675e0f3
Stephen Agyemang <agyemangstephen2580@gmail.com>
2026-09-13 01:57:27 +0000

    Sync LeetCode submission Runtime - 67 ms (42.32%), Memory - 28.8 MB (93.03%)

--- 0643-maximum-average-subarray-i/solution.py
+++ 0643-maximum-average-subarray-i/solution.py
@@ -1,24 +1,15 @@
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
 
-        right = k - 1
-        left = 0
-
-        curr_sum = sum(nums[left:right + 1])
+        curr_sum = sum(nums[:k])
         max_sum = curr_sum
+        left = 0 
+        # right = k
 
-        while right < len(nums) - 1:
-            curr_sum -= nums[left]
-
-            right += 1
-            left += 1
-
-            curr_sum += nums[right]
-
+        for i in range(k, len(nums)):
+            curr_sum += nums[i] - nums[left]
             max_sum = max(max_sum, curr_sum)
+            left += 1
+            # right += 1
 
-        return max_sum / k
-            
-
-
-        
+        return max_sum/k
