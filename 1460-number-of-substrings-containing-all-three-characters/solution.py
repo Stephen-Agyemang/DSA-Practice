e95a1169e155e0137502b31f88d9f8def67a3d91
Stephen Agyemang <agyemangstephen2580@gmail.com>
2026-09-18 22:04:24 +0000

    Sync LeetCode submission Runtime - 66 ms (83.07%), Memory - 19.4 MB (85.92%)

--- 1460-number-of-substrings-containing-all-three-characters/solution.py
+++ 1460-number-of-substrings-containing-all-three-characters/solution.py
@@ -1,22 +1,18 @@
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
-        
-        count_substring = 0
-        dct = {'a': -1, 'b': -1, 'c': -1}
 
+        abc = {'a': -1, 'b': -1, 'c': -1}
+
+        num_substrings = 0 
         for i, char in enumerate(s):
+            abc[char] = i
 
-            dct[char] = i
-            
-            if dct['a'] != -1 and dct['b'] != -1 and dct['c'] != -1:
-                farthest_left_boundary = min(dct['a'], dct['b'], dct['c'])
+            min_idx = min(abc['a'], abc['b'], abc['c'])
 
-                count_substring += farthest_left_boundary + 1
+            if min_idx >= 0:
+                num_substrings += min_idx + 1
 
-        return count_substring
-
+        return num_substrings
 
 
-                
 
-        
